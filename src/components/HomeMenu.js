717fd3d664184b758e34865ce8de448ea452e380
archymi <archymi77@gmail.com>
2020-11-08 16:14:39 +0900

    UI : 201108

--- src/components/HomeMenu.js
+++ src/components/HomeMenu.js
@@ -14,21 +14,38 @@ function HomeMenu() {
                     <img className={cx('logoImg')} src={logoImg} alt='logo' />
                     <img className={cx('logoText')} src={logoText} alt='logo' />
                 </button>
-                <nav className={cx('gnb')}>
+                <nav className={cx('gnb', 'no-drag')}>
                     <ul className={cx('gnbList')}>
-                        <li className={cx('spotLight')}>
+                        <li className={cx('menuList', 'spotLight')}>
                             LED SPOT LIGHT
+                            <div className={cx('spotInner', 'Inner01')}>
+                                <li>스포트 12 ~ 15W</li>
+                                <li>스포트 21 ~ 25W</li>
+                                <li>스포트 30 ~ 40W</li>
+                                <li>스포트 50W</li>
+                            </div>
                         </li>
-                        <li className={cx('downLight')}>
-                            LED DOWN LIGHT
+                        <li className={cx('menuList', 'downLight')}>
+                            LED 다운라이트
+                            <div className={cx('spotInner', 'Inner02')}>
+                                <li>45타공 이하</li>
+                                <li>55 ~ 65 타공</li>
+                                <li>70 ~ 80 타공</li>
+                                <li>50 ~ 70 타공</li>
+                                <li>70 ~ 80 타공</li>
+                                <li>80 ~ 90 타공</li>
+                                <li>90 ~ 100 타공</li>
+                                <li>105 ~ 125 타공</li>
+                                <li>130 타공이상</li>
+                            </div>
                         </li>
-                        <li className={cx('halogenLight')}>
-                            Halogen DOWN LIGHT
+                        <li className={cx('menuList', 'halogenLight')}>
+                            할로겐 다운라이트
                         </li>
-                        <li className={cx('product')}>
+                        <li className={cx('menuList', 'product')}>
                             Product
                         </li>
-                        <li className={cx('hotSale')}>
+                        <li className={cx('menuList', 'hotSale')}>
                             HOT SALE
                         </li>
                     </ul>
